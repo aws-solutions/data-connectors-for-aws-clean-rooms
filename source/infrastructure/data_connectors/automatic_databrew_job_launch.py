225fc041dc8ca6c98a950d365ba3be418d89a5ac
yang qin <yangqqin@amazon.com>
2023-02-17 10:48:34 -0600

    Fix circular dependency

--- source/infrastructure/data_connectors/automatic_databrew_job_launch.py
+++ source/infrastructure/data_connectors/automatic_databrew_job_launch.py
@@ -32,20 +32,19 @@ from data_connectors.aws_lambda import LAMBDA_PATH
 
 class AutomaticDatabrewJobLaunch:
     def __init__(self, stack, schema_provider_parameter=None) -> None:
-        stack_account_id: str = Aws.ACCOUNT_ID
 
         self.create_template_parameters(stack)
 
+        self.create_s3_notifications_queue(stack)
+
         self.create_lambda_iam_policy(
-            stack, stack_account_id,
+            stack,
             stack.dynamodb_table.table_name,
             stack.workflow.state_machine.state_machine_name
         )
 
-        self.create_s3_notification_lambda_function(stack, self.automatic_brew_job_launch,
+        self.create_lambda_processing_notifications(stack, self.automatic_brew_job_launch,
                                                     self.file_upload_complete_waiting_time_in_minutes)
-
-        self.create_s3_notifications_queue(stack)
 
         self.add_s3_notifications_to_sqs(stack)
         self.add_lambda_event_source_sqs()
@@ -80,11 +79,9 @@ class AutomaticDatabrewJobLaunch:
             group=group_name
         )
 
-    def create_lambda_iam_policy(self, stack, stack_account, dynamodb_table_name, state_machine_name):
-        policy_statements: list[iam.PolicyStatement] = self.create_policy_statements_for_lambda(
-            stack_account,
-            dynamodb_table_name,
-            state_machine_name)
+    def create_lambda_iam_policy(self, stack, dynamodb_table_name, state_machine_name):
+        policy_statements: list[iam.PolicyStatement] = self.create_policy_statements_for_lambda(dynamodb_table_name,
+                                                                                                state_machine_name)
 
         self.lambda_iam_policy = iam.Policy(stack, "ProcessS3NotificationsLambdaIamPolicy",
                                             statements=policy_statements)
@@ -92,7 +89,7 @@ class AutomaticDatabrewJobLaunch:
 
         self.lambda_iam_policy_cdk_nag_suppresions(self.lambda_iam_policy)
 
-    def create_s3_notification_lambda_function(self, stack, automatic_brew_job_launch,
+    def create_lambda_processing_notifications(self, stack, automatic_brew_job_launch,
                                                file_upload_complete_waiting_time_in_minutes):
         self.lambda_process_s3_notification = SolutionsPythonFunction(
             stack,
@@ -142,8 +139,6 @@ class AutomaticDatabrewJobLaunch:
             visibility_timeout=Duration.seconds(SQSQueueParameters.visibility_timeout_in_seconds),
             queue_name=f"{Aws.STACK_NAME}-s3-notifications",
         )
-        self.s3_notifications_queue.node.add_dependency(self.lambda_process_s3_notification)
-        self.s3_notifications_queue.node.add_dependency(stack.connector_buckets.inbound_bucket)
 
     def add_s3_notifications_to_sqs(self, stack):
         stack.connector_buckets.inbound_bucket.add_event_notification(
@@ -163,7 +158,7 @@ class AutomaticDatabrewJobLaunch:
             )
         )
 
-    def create_policy_statements_for_lambda(self, stack_account, dynamodb_table_name, state_machine_name):
+    def create_policy_statements_for_lambda(self, dynamodb_table_name, state_machine_name):
         dynamodb_table_statement = iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
             actions=[
@@ -173,9 +168,9 @@ class AutomaticDatabrewJobLaunch:
                 "dynamodb:GetItem",
             ],
             resources=[
-                f"arn:aws:dynamodb:*:{stack_account}:table/{dynamodb_table_name}/stream/*",
-                f"arn:aws:dynamodb:*:{stack_account}:table/{dynamodb_table_name}",
-                f"arn:aws:dynamodb:*:{stack_account}:table/{dynamodb_table_name}/index/*"
+                f"arn:aws:dynamodb:*:{Aws.ACCOUNT_ID}:table/{dynamodb_table_name}/stream/*",
+                f"arn:aws:dynamodb:*:{Aws.ACCOUNT_ID}:table/{dynamodb_table_name}",
+                f"arn:aws:dynamodb:*:{Aws.ACCOUNT_ID}:table/{dynamodb_table_name}/index/*"
             ]
         )
 
@@ -188,9 +183,9 @@ class AutomaticDatabrewJobLaunch:
                 "states:ListExecutions",
             ],
             resources=[
-                f"arn:aws:states:*:{stack_account}:activity:{state_machine_name}:*",
-                f"arn:aws:states:*:{stack_account}:stateMachine:{state_machine_name}",
-                f"arn:aws:states:*:{stack_account}:execution:{state_machine_name}:*"
+                f"arn:aws:states:*:{Aws.ACCOUNT_ID}:activity:{state_machine_name}:*",
+                f"arn:aws:states:*:{Aws.ACCOUNT_ID}:stateMachine:{state_machine_name}",
+                f"arn:aws:states:*:{Aws.ACCOUNT_ID}:execution:{state_machine_name}:*"
             ]
         )
 
